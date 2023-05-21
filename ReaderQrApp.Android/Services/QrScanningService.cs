bf93c0e0d4a288d89e0979c39aa070eb5d869c6a
Edison <ayoriachagua@gmail.com>
2023-05-21 12:28:10 -0500

    Agregar archivos de proyecto.

---
+++ ReaderQrApp.Android/Services/QrScanningService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using ZXing.Mobile;
+using Xamarin.Forms;
+using ReaderQrApp.Services;
+
+[assembly: Dependency(typeof(ReaderQrApp.Droid.Services.QrScanningService))]
+namespace ReaderQrApp.Droid.Services
+{
+    public class QrScanningService : IQrScanningService
+    {
+        public async Task<string> ScanAsync()
+        {
+            var options = new MobileBarcodeScanningOptions();
+            var scanner = new MobileBarcodeScanner();
+
+            var context = Xamarin.Essentials.Platform.CurrentActivity; // Obtener el contexto actual
+
+            var scanResult = await scanner.Scan(context, options);
+            return scanResult.Text;
+        }
+    }
+}
---
+++ ReaderQrApp/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ReaderQrApp.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ReaderQrApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnScan_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var scanner = DependencyService.Get<IQrScanningService>();
+                var result = await scanner.ScanAsync();
+                if (result != null)
+                {
+                    txtBarcode.Text = result;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
